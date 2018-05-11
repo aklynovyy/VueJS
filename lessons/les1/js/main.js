5a0ad3974cbb3d8c67cc795691b8eac0d37a8050
unknown <aklynovyy@gmail.com>
2018-05-11 17:57:14 +0300

    Update Lessons

--- lessons/les1/js/main.js
+++ lessons/les1/js/main.js
@@ -4,3 +4,12 @@ new Vue ({
  		title: 'Hallo World!!!'
  	}
 });
+
+
+new Vue ({
+	el: '#appHome',
+	data: {
+		title: 'Hi World',
+		title2: 'Hi World 2222'
+	}
+});
--- lessons/les2/js/main.js
+++ lessons/les2/js/main.js
@@ -2,11 +2,32 @@ new Vue ({
  	el: '#app',
  	data: {
  		title: 'Hallo World!!!',
- 		styleCSS: ''
+ 		className: ''
  	},
  	methods: {
  		changeText () {
- 			this.title = 'NewText'
+ 			this.title = 'New text after click'
  		}
  	}
+ 	
 });
+
+// new Vue ({
+// 	el: '#app-home-1',
+// 		data: {
+// 		beforeText: "Some Text",
+// 		inputText: ''
+// 	},
+// 	methods: {
+// 		sendText () {
+// 			this.BeforeText = this.inputText
+// 		}
+// 	}
+// });
+
+new Vue ({
+	el: '#app-home-2',
+	data: {
+		isDisabled: false
+	}
+})
